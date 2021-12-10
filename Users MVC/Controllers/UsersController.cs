f3424ee3f203b4fc660c9d7e21bab86ee9fccb76
Cristian <cristianoteroadsi@gmail.com>
2021-12-09 19:54:10 -0500

    first commit

---
+++ Users MVC/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Users_MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        //[HttpGet]
+        public IActionResult Index(string name, int age)
+        {
+            string Data = name+" "+age;
+            return View("Index", Data);
+        }
+    }
+}
