de4d66dac4965b7f6016a3abf65ded7c663b3245
Cristian <cristianoteroadsi@gmail.com>
2021-12-10 20:21:07 -0500

    First Commit

--- Users MVC/Controllers/UsersController.cs
+++ Users MVC/Controllers/UsersController.cs
@@ -2,13 +2,20 @@
 
 namespace Users_MVC.Controllers
 {
+    [Route("/Usuarios")]
     public class UsersController : Controller
     {
         //[HttpGet]
+        [Route("/Usuarios/home")]
+        [Route("/Usuarios/index/{name")]
         public IActionResult Index(string name, int age)
         {
             string Data = name+" "+age;
             return View("Index", Data);
+        }
+        public IActionResult Index()
+        { 
+            return View();
         }
     }
 }
