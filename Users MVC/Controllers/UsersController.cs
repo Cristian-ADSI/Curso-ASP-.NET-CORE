e2863331e75f85571147215562caa7c9374b7394
Cristian <cristianoteroadsi@gmail.com>
2021-12-13 08:19:37 -0500

    13/12/2021

--- Users MVC/Controllers/UsersController.cs
+++ Users MVC/Controllers/UsersController.cs
@@ -2,33 +2,22 @@
 
 namespace Users_MVC.Controllers
 {
-<<<<<<< HEAD
- 
+
+
     public class UsersController : Controller
     {
-        
-        public IActionResult Index()
-=======
-    //[Route("/Usuarios")]
-    public class UsersController : Controller
-    {
-        //[HttpGet]
-        [Route("/Usuarios/home")]
-        [Route("/[controller]/[action]/{name}")]
+
         public IActionResult Index(string name, int age)
->>>>>>> 3cebf87a9c36ba98afec61a2f3656f538b735c5d
         {
-            //var url = Url.Action("Method", "Users", new {age = 52, name =  "Jhon"});
             var url = Url.RouteUrl("Usuarios", new { age = 52, name = "Jhon" });
             return Redirect(url);
         }
 
         [HttpGet("[controller]/[action]", Name = "Usuarios")]
         public IActionResult Method(string name, int age)
-        {   
+        {
             var data = $"name {name} age {age}";
             return View("Index", data);
         }
-        
-    }
+    }   
 }
