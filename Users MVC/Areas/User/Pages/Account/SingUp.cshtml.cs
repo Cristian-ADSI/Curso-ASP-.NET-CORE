bc8267c08803ffa07aa5c9a2b491569c1965450b
Cristian <cristianoteroadsi@gmail.com>
2021-12-20 20:35:22 -0500

    20/12/2021
    Sing Up Form

--- Users MVC/Areas/User/Pages/Account/SingUp.cshtml.cs
+++ Users MVC/Areas/User/Pages/Account/SingUp.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Users_MVC.Areas.User.Pages.Account
 {
     public class SingUpModel : PageModel
     {
+
         public void OnGet()
         {
+        }
+
+        [BindProperty]
+        public ImputModel Imput { get; set; }
+        public class ImputModel{
+            [Required]
+            [EmailAddress]
+            [Display (Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [Display (Name = "Key")]
+            [DataType(DataType.Password)]
+            [StringLength(100, ErrorMessage = " The number of characters of {0} must be almost {2}.", MinimumLength = 8)] 
+            public string Password { get; set; }
+
+
+            [Required]
+            [Display(Name = "Confirm Key")]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage ="The password & conrfim password don`t match, please verify it")]
+            public string Confirmassword { get; set; }
+
         }
     }
 }
